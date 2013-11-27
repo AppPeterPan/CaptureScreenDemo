298596f83a345e23be642740751edd1ed5e75ca2
Peter Pan <apppeterpan@gmail.com>
2013-11-27 15:23:36 +0800

    Initial Commit

---
+++ CaptureScreenDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CaptureScreenDemo
+//
+//  Created by Peter Pan on 11/27/13.
+//  Copyright (c) 2013 Peter Pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
